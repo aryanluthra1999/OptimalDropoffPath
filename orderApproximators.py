6b20769047de049615b2c24bda6ef1fa5beebf1e
Richard Padilla <padillarichard2021@berkeley.edu>
2019-12-04 17:58:34 -0800

    output progress

--- orderApproximators.py
+++ orderApproximators.py
@@ -44,7 +44,7 @@ class OrderApproximator:
 
         order = self.get_dropoff_ordering()
         order.append(self.start_loc)
-
+        result.append(self.start_loc)
         for i in range(len(order) - 1):
             home = order[i]
             next_home = order[i+1]
@@ -65,13 +65,31 @@ class OrderApproximator:
                 else:
                     result.append(node)
                     curr_loc = node
-
+        locs=[]
+        dropoffs=dict()
+        curr='b'
+        for i in range(len(result)):
+            if i!=0 and result[i-1][0:8]=='drop off':
+                continue
+            else:
+                if(len(result[i])==7 and result[i]=='go_home'):
+                    break
+                if (len(result[i])<8):
+                    curr=result[i]
+                    locs+=[curr]
+                if len(result[i])>8 and result[i][0:8]=='drop off':
+                    if curr not in dropoffs.keys():
+                        dropoffs[curr]=[result[i][9:]]
+                    else:
+                        dropoffs[curr]=dropoffs[curr]+[result[i][9:]]
+                else:
+                    curr=result[i]
+                    locs+=[curr]
+        back_home=netx.shortest_path(self.graph,locs[len(locs)-1],locs[0])
+        print(back_home)
+        for b in back_home:
+            locs.append(b)
+        print(dropoffs)
+        print(locs)
         print(result)
         return result
-
-
-
-
-
-
-
