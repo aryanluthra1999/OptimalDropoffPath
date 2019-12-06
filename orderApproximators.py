af8999d981ef919bf244160b852400d1f1ddb6da
Aryan Luthra <aryanluthra@berkeley.edu>
2019-12-06 13:42:12 -0800

    updated_outputs

--- orderApproximators.py
+++ orderApproximators.py
@@ -13,10 +13,45 @@ from itertools import chain, combinations
 import copy
 import time
 import random
-from student_utils import cost_of_solution
 
 #import pdb
 from tqdm import tqdm
+
+
+def cost_of_solution(G, car_cycle, dropoff_mapping, shortest):
+    cost = 0
+    message = ''
+    dropoffs = dropoff_mapping.keys()
+    if not util170.is_valid_walk(G, car_cycle):
+        message += 'This is not a valid walk for the given graph.\n'
+        cost = 'infinite'
+
+    if not car_cycle[0] == car_cycle[-1]:
+        message += 'The start and end vertices are not the same.\n'
+        cost = 'infinite'
+    if cost != 'infinite':
+        if len(car_cycle) == 1:
+            car_cycle = []
+        else:
+            car_cycle = util170.get_edges_from_path(car_cycle[:-1]) + [(car_cycle[-2], car_cycle[-1])]
+        if len(car_cycle) != 1:
+            driving_cost = sum([G.edges[e]['weight'] for e in car_cycle]) * 2 / 3
+        else:
+            driving_cost = 0
+        walking_cost = 0
+        #shortest = dict(netx.floyd_warshall(G))
+
+        for drop_location in dropoffs:
+            for house in dropoff_mapping[drop_location]:
+                walking_cost += shortest[drop_location][house]
+
+        message += f'The driving cost of your solution is {driving_cost}.\n'
+        message += f'The walking cost of your solution is {walking_cost}.\n'
+        cost = driving_cost + walking_cost
+
+    message += f'The total cost of your solution is {cost}.\n'
+    return cost, message
+
 
 class OrderApproximator:
 
@@ -24,10 +59,14 @@ class OrderApproximator:
         self.graph = util170.adjacency_matrix_to_graph(adj_matrix)[0]  ## maybe we want a graph object from network x instead
         mapping = dict(zip(self.graph,locs))
         self.graph = netx.relabel_nodes(self.graph, mapping)
-        self.distanceMemo = dict()
         self.start_loc = soda_loc
         self.homes = homes_arr
         self.locs=locs
+        self.distanceMemo = dict()
+
+        self.distancePathMemo = dict(netx.algorithms.shortest_paths.all_pairs_dijkstra_path(self.graph))
+        self.distanceLengthMemo = dict(netx.algorithms.shortest_paths.all_pairs_dijkstra_path_length(self.graph))
+        print("finished_memoizing_shortest_paths")
 
 
 
@@ -51,7 +90,7 @@ class OrderApproximator:
             else:
                 curr=result[i]
                 locs.append(curr)
-        back_home=netx.shortest_path(self.graph,locs[len(locs)-1],locs[0])
+        back_home = self.distancePathMemo[locs[len(locs)-1]][locs[0]]
         #print(back_home)
         for i in range(len(back_home)):
             if i==0:
@@ -102,6 +141,14 @@ class OrderApproximator:
 
         return final_order
 
+    def distance(self, source, target):
+        k = (source, target)
+        if k in self.distanceMemo:
+            return self.distanceMemo[k]
+        else:
+            result = netx.dijkstra_path(self.graph, source, target), netx.dijkstra_path_length(self.graph, source, target)
+            self.distanceMemo[k] = result
+            return result
 
     def get_drop_path(self, tree_func = get_dropoff_ordering_steiner):
 
@@ -118,18 +165,18 @@ class OrderApproximator:
 
         for i in range(len(order) - 1):
             home = order[i]
-            next_home = order[i+1]
+            next_home = order[i + 1]
 
-            shortest_path = netx.shortest_path(self.graph, source=curr_loc, target=home)
+            shortest_path = self.distancePathMemo[curr_loc][home]
 
             for node in shortest_path:
                 curr_loc = node
 
-                cont_path_cost = 2/3*(netx.dijkstra_path_length(self.graph, source=curr_loc, target=home)
-                                      + netx.dijkstra_path_length(self.graph, source=home, target=next_home))
+                cont_path_cost = 2 / 3 * (self.distanceLengthMemo[curr_loc][home]
+                                          + self.distanceLengthMemo[home][next_home])
 
-                drop_path_cost = (netx.dijkstra_path_length(self.graph, source=curr_loc, target=home)
-                                  + 2/3*(netx.dijkstra_path_length(self.graph, source=curr_loc, target=next_home)))
+                drop_path_cost = (self.distanceLengthMemo[curr_loc][home]
+                                  + 2 / 3 * self.distanceLengthMemo[curr_loc][next_home])
 
                 if drop_path_cost <= cont_path_cost:
                     result.append("drop off " + str(home))
@@ -180,18 +227,18 @@ class OrderApproximator:
 
         for i in range(len(order) - 1):
             home = order[i]
-            next_home = order[i+1]
+            next_home = order[i + 1]
 
-            shortest_path = netx.shortest_path(self.graph, source=curr_loc, target=home)
+            shortest_path = self.distancePathMemo[curr_loc][home]
 
             for node in shortest_path:
                 curr_loc = node
 
-                cont_path_cost = 2/3*(netx.dijkstra_path_length(self.graph, source=curr_loc, target=home)
-                                      + netx.dijkstra_path_length(self.graph, source=home, target=next_home))
+                cont_path_cost = 2 / 3 * (self.distanceLengthMemo[curr_loc][home]
+                                          + self.distanceLengthMemo[home][next_home])
 
-                drop_path_cost = (netx.dijkstra_path_length(self.graph, source=curr_loc, target=home)
-                                  + 2/3*(netx.dijkstra_path_length(self.graph, source=curr_loc, target=next_home)))
+                drop_path_cost = (self.distanceLengthMemo[curr_loc][home]
+                                  + 2 / 3 * self.distanceLengthMemo[curr_loc][next_home])
 
                 if drop_path_cost <= cont_path_cost:
                     result.append("drop off " + str(home))
@@ -208,24 +255,24 @@ class OrderApproximator:
         return self.get_path_dropoffs(result)
 
 
-    def steiner_aneal(self, iterations = 500, tree_func = get_dropoff_ordering_steiner, epsilon = 0):
+    def steiner_aneal(self, iterations = 50000, tree_func = get_dropoff_ordering_steiner, epsilon = 0):
 
         curr_order = tree_func(self)
         curr_result = self.get_drop_path_with_order(curr_order)
-        curr_min = cost_of_solution(self.graph, curr_result[0], curr_result[1])
+        curr_min = cost_of_solution(self.graph, curr_result[0], curr_result[1], self.distanceLengthMemo)
         # print("initial cost ", curr_min)
 
         i = 0
 
         while i <= (iterations):
-            print(i)
+            #print(i)
 
             e_prob = epsilon/max(1, i/10)
 
 
             new_order = self.mutate_order(curr_order)
             new_result = self.get_drop_path_with_order(new_order)
-            new_cost = cost_of_solution(self.graph, new_result[0], new_result[1])
+            new_cost = cost_of_solution(self.graph, new_result[0], new_result[1], self.distanceLengthMemo)
 
             # print(new_order)
 
@@ -236,7 +283,7 @@ class OrderApproximator:
 
                 i = 0
 
-            i+=1
+            i += 1
 
 
         return curr_result
@@ -259,7 +306,7 @@ class OrderApproximator:
     def mst_aneal(self, iterations=100, tree_func=get_dropoff_ordering_mst, epsilon = 0.7):
         curr_order = tree_func(self)
         curr_result = self.get_drop_path_with_order(curr_order)
-        curr_min = cost_of_solution(self.graph, curr_result[0], curr_result[1])
+        curr_min = cost_of_solution(self.graph, curr_result[0], curr_result[1], self.distanceLengthMemo)
         # print("initial cost ", curr_min)
 
         for i in (range(iterations)):
@@ -268,7 +315,7 @@ class OrderApproximator:
 
             new_order = self.mutate_order(curr_order)
             new_result = self.get_drop_path_with_order(new_order)
-            new_cost = cost_of_solution(self.graph, new_result[0], new_result[1])
+            new_cost = cost_of_solution(self.graph, new_result[0], new_result[1], self.distanceLengthMemo)
 
             if new_cost < curr_min or random.random() < e_prob:
                 curr_order = new_order
@@ -276,3 +323,5 @@ class OrderApproximator:
                 curr_min = new_cost
 
         return curr_result
+
+
